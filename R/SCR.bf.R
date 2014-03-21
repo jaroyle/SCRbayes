b9504c98d201d340bf455526a7e0a408cfe94099
joshua-goldberg <joshua.goldberg@umontana.edu>
2014-03-20 23:42:05 -0600

    updated model selection functions

--- R/SCR.bf.R
+++ R/SCR.bf.R
@@ -10,12 +10,13 @@
 SCR.bf = function(modelsin, refmodel=NULL){
 	require(mvtnorm)
 	nmodels = length(modelsin)
-	model.lik <-model.lik2 <- cumLL<- maxL <-avgL<- medL<- rep(NA, nmodels)
-	LLsamples <-LLs<- matrix(nrow=length(modelsin[[1]]$Lout),ncol=nmodels)
+	model.lik <- rep(NA, nmodels)
+	n.nolik <- list()
+#	LLsamples <-LLs<- matrix(nrow=length(modelsin[[1]]$Lout),ncol=nmodels)
 	for (i in 1:nmodels){
 		model = modelsin[[i]]
-		model.post = model$out[,model$parms2report]
-		model.post = model.post[,colnames(model.post)!="sigma"&colnames(model.post)!="sigma2" &colnames(model.post)!="D"]
+		model.post = model$mcmchist[,model$parms2report]
+		model.post = model.post[,colnames(model.post)!="sigma"&colnames(model.post)!="sigma2" &colnames(model.post)!="D"&colnames(model.post)!="lam0"]
 		
 		post.avg = apply(model.post,2,mean)
 		post.var = var(model.post)
@@ -28,14 +29,16 @@ SCR.bf = function(modelsin, refmodel=NULL){
 			chain.dens[,c]=emp.dens[[c]][,2][findInterval(model.post[,c],emp.dens[[c]][,1])]
 		}
 		f.samples = dmvt(model.post,delta=post.avg, sigma=cov2cor(post.var), df=nrow(model.post),log=T)
-		model.lik[i] = (1/nrow(model.post)*sum(exp(f.samples-(model$Lout+log(apply(chain.dens,1,prod))))))^(-1)
-		
+		model.lik[i] = (1/nrow(model.post)*sum(exp(f.samples-(model$likelihood[,1]+log(apply(chain.dens,1,prod))))))^(-1)
+		n.nolik[[i]] <- c("sum"=sum(model$likelihood[,2]), "mean"=mean(model$likelihood[,2]), "median"=median(model$likelihood[,2]), min = min(model$likelihood[,2]), "max"=max(model$likelihood[,2]))
 #		maxL[i] = max(model$likelihood)
 #		avgL[i] = mean(model$likelihood) 
 #		medL[i] = median(model$likelihood)
 	}
 	post.prob =(model.lik)/sum((model.lik))
 	names(model.lik) <- names(modelsin)
+	n.nolik.df = do.call("rbind", n.nolik)
+
 	if(!is.null(refmodel)){
 	bf = model.lik/model.lik[refmodel]
 	Jeff.breaks = c(1,3,10,30,100)
@@ -46,9 +49,13 @@ SCR.bf = function(modelsin, refmodel=NULL){
 	out.table$JeffreysEvidence = factor(out.table$JeffreysEvidence,levels = c(levels(out.table$JeffreysEvidence),"Null Model"))	
 	out.table$KRevidence = factor(out.table$KRevidence,levels = c(levels(out.table$KRevidence),"Null Model"))	
 	out.table[refmodel,c("JeffreysEvidence","KRevidence")]="Null Model"
-	return(out.table)
+	rownames(out.table)=NULL
+	out = list("ModelSelection"=out.table, "LowLikelihoodObservations"=n.nolik.df)
+	return(out)
 	} else {
 		out.table = data.frame("Model" = names(modelsin), "Post.Prob" = post.prob)
-		return (out.table)
+		rownames(out.table)=NULL
+		out = list("ModelSelection"=out.table, "LowLikelihoodObservations"=n.nolik.df)
+		return (out)
 	}
 }
--- R/SCR.bic.R
+++ R/SCR.bic.R
@@ -7,31 +7,36 @@
 # is independent... this may require some adjustment, but I have not found 
 # an easy way of computing multivariate empirical PDFs. 
 
-SCR.bic = function(modelsin,ncaps, refmodel=NULL){
+SCR.bic = function(modelsin,ncaps, sort=TRUE){
 	
 	nmodels = length(modelsin)
-	model.loglik <- avg.loglik<- min.loglik <- nparam <- model.npts <- rep(NA, nmodels)
+	model.loglik <- model.npts <- nparam <- rep(NA, nmodels)
+	n.nolik <- list()
 	for (i in 1:nmodels){
 		model = modelsin[[i]]
 		model.loglik[i] = max(model$likelihood[,1])
 		model.npts[i] = model$likelihood[which(model$likelihood[,1]==max(model$likelihood[,1])),2]
-		avg.loglik[i] = mean(model$likelihood[,1])
-		min.loglik[i] = min(model$likelihood[,1])
-		hist(model$likelihood, xlab = "Log Likelihood", main = names(modelsin)[i])
+#		hist(model$likelihood, xlab = "Log Likelihood", main = names(modelsin)[i])
 		model.post = model$mcmchist[,model$parms2report]
-		model.post = model.post[,colnames(model.post)!="sigma"&colnames(model.post)!="sigma2" &colnames(model.post)!="D"]
+		model.post = model.post[,colnames(model.post)!="sigma"&colnames(model.post)!="sigma2" &colnames(model.post)!="D"&colnames(model.post)!="lam0"]
 		nparam[i] = ncol(model.post)
+		n.nolik[[i]] <- c("sum"=sum(model$likelihood[,2]), "mean"=mean(model$likelihood[,2]), "median"=median(model$likelihood[,2]), min = min(model$likelihood[,2]), "max"=max(model$likelihood[,2]))
 		}
 	BIC = -2*model.loglik + nparam*log(ncaps)
-	deltaBIC = ifelse(rep(is.null(refmodel),nmodels), BIC-min(BIC), BIC-BIC[refmodel])
-	wBIC = exp(-0.5*deltaBIC)/exp(sum(-0.5*deltaBIC))
+	names(BIC) <- names(modelsin)
+	deltaBIC = BIC-min(BIC)
+	wBIC = exp(-0.5*deltaBIC)/sum(exp(-0.5*deltaBIC))
 	
-	KR.breaks = c(1,3,20,150)
-	KR.evid = c("Supports Null","Barely worth mentioning", "Positive","Strong","Very Strong")
+	n.nolik.df = do.call("rbind", n.nolik)
 	
-	out.table = data.frame("Model" = names(modelsin), "MaxlnL" = model.loglik, "BIC"=BIC, "deltaBIC" = deltaBIC, "wBIC" = wBIC,"No.Obs.Unexplained" = model.npts,"KRevidence" = KR.evid[findInterval(-deltaBIC, KR.breaks)+1])
-	out.table$KRevidence = factor(out.table$KRevidence,levels = c(levels(out.table$KRevidence),"Reference Model"))	
-	out.table[ifelse(is.null(refmodel), which(BIC==min(BIC)),refmodel),c("KRevidence")]="Reference Model"
-	return(out.table)
+
+	
+	out.table = data.frame("Model" = names(modelsin), "MaxlnL" = model.loglik, "BIC"=BIC, "deltaBIC" = deltaBIC, "wBIC" = wBIC,"No.Obs.Unexplained" = model.npts)
+	rownames(out.table)=NULL
+	if(sort){
+		out.table = out.table[order(deltaBIC),]
+	}
+	out = list("ModelSelection"=out.table, "LowLikelihoodObservations"=n.nolik.df)
+	return(out)
 	
 }
