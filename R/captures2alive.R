ae2714f98d58171dbd61e3ba68474c9968458176
joshua-goldberg <joshua.goldberg@umontana.edu>
2014-03-21 04:02:09 -0600

    captures2alive updates

--- R/captures2alive.R
+++ R/captures2alive.R
@@ -2,8 +2,8 @@ captures2alive <- function(scrCaptures, status){
 	alive = matrix(1,nrow=max(scrCaptures$individual),ncol=max(scrCaptures$occasion))
 	dead.col = scrCaptures$occasion[status=="Dead"]+1
 	dead.row = scrCaptures$individual[(status=="Dead")]
-	rep.deadr = rep(dead.row[dead.col<4],times = (5-dead.col[dead.col<4]))
-	rep.deadc = unlist(lapply(dead.col[dead.col<4],function(x){seq(x,4,1)}))
+	rep.deadr = rep(dead.row[dead.col<(max(scrCaptures$occasion)+1)],times = ((max(scrCaptures$occasion)+1)-dead.col[dead.col<(max(scrCaptures$occasion)+1)]))
+	rep.deadc = unlist(lapply(dead.col[dead.col<(max(scrCaptures$occasion)+1)],function(x){seq(x,max(scrCaptures$occasion),1)}))
 	alive[cbind(rep.deadr,rep.deadc)]=0
 	return(alive)
 }
